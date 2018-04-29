c136e6b7c096fdb9e9c3814541abb5d8ad27d840
Dmitry Poponkin <mitia.poponkin@yandex.ru>
2018-04-29 16:54:49 +0300

    fixed dockerfile [18]

--- src/github.com/pdmitrya/goExample/main.go
+++ src/github.com/pdmitrya/goExample/main.go
@@ -32,5 +32,5 @@ func main() {
     router.POST("/service/clear", routes.ClearService)
     router.GET("/service/status", routes.StatusService)
 
-    log.Fatal(http.ListenAndServe(":5001", router))
+    log.Fatal(http.ListenAndServe(":5000", router))
 }
